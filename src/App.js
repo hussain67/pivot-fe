f8def83c2bcdb8186902b695b4f2a5907afa643e
Roshni <roshnigupta8@gmail.com>
2022-03-01 12:11:53 +0000

    changed socket io connection

--- src/App.js
+++ src/App.js
@@ -13,11 +13,9 @@ let socket = io.connect("https://rhs-pivot-backend.herokuapp.com");
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
   // dev code
   socket = io.connect("http://localhost:9090")
-  console.log('9090 connection')
 } else {
   // production code
   socket = io.connect("https://rhs-pivot-backend.herokuapp.com")
-  console.log('backend deployed port')
 }
 
 console.log(socket)
